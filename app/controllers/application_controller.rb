46a912d87020781db0035ed8bbc2826f2413f463
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-23 06:01:27 -0500

    Copied from sinatra secure lab

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,72 @@
+require "./config/environment"
+require "./app/models/user"
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    username = params[:username]
+    password = params[:password]
+    if !username || username.length == 0 || !password || password.length == 0
+      puts "redirect no password"
+      redirect "/failure"
+    else 
+      puts "go to login"
+      user = User.new(:username => username, :password => password)
+      user.save
+      redirect "/login"
+    end
+  end
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :account
+  end
+
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/account"
+      else
+        redirect "/failure"
+      end
+    end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
+end
---
+++ db/migrate/001_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def up 
+    create_table :users do |t| 
+      t.string :username
+      t.string :password
+    end
+  end
+
+  def down
+  end
+end
