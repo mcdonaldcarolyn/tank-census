6905aac40a2b972c3380df887b94e5bb6c82fdcd
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-02-04 12:07:46 -0500

    fixed typo in get signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
       erb :'/users/signup'
     else 
       @user = User.create(:username => params[:username], :password => params[:password])
-      session[user] = @user
+      session[username] = @user
       session[user_id] = @user.id 
       if @user.valid?
         #@user.save
